6a29d5eacc1633dd87fba5719456cccaf29c8dc8
ashmit1002 <ashmits1002@gmail.com>
2025-07-09 17:34:18 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_headers=["*"]
 )
 
 # Include routes
