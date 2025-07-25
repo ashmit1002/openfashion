16e5f4f8f0d5ec23223e218803eb315ec9eaa889
ashmit1002 <ashmits1002@gmail.com>
2025-07-24 23:56:14 -0400

    CORS update

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=[
+        "https://openfashion.vercel.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
